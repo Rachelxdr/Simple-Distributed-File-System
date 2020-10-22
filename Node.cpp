1928b1b6f6d30a5e54ff50ae288c28c6ede1c0b6
Duorui Xu <duoruixu@Duoruis-MacBook-Pro.local>
2020-10-22 17:43:10 -0500

    update time

--- Node.cpp
+++ Node.cpp
@@ -93,8 +93,8 @@ void Node::send_message(string ip, string port, Message* msg_to_send) {
     memset(&hints, 0, sizeof(hints));
 
     hints.ai_family = AF_UNSPEC;
-    // hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_socktype = SOCK_DGRAM;
+    // hints.ai_socktype = SOCK_STREAM;
 
     // cout << "calling get addr info"<<endl;
     int get_addr_info_ret = getaddrinfo(ip.c_str(), port.c_str(), &hints, &servinfo);
