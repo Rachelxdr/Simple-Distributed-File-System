942706d51232cf996d94509f308c231e1131d74c
Duorui Xu <duoruixu@wirelessprv-10-192-118-174.near.illinois.edu>
2020-10-22 16:03:40 -0500

    fixed local time

--- Node.cpp
+++ Node.cpp
@@ -92,8 +92,8 @@ void Node::send_message(string ip, string port, Message* msg_to_send) {
     
     memset(&hints, 0, sizeof(hints));
 
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
     // hints.ai_socktype = SOCK_STREAM;
 
     // cout << "calling get addr info"<<endl;
@@ -319,6 +319,7 @@ void Node::read_message(string msg){
 int Node::get_time() {
     time_t time_buf;
     time(&time_buf);
+    cout <<"time function: "<<time_buf<<endl;
     return static_cast<int>(time_buf);
 }
 
